46d4ac5404f7bcc3e000943bc4e4a82df7954c57
Anass <146187264+bokettoo@users.noreply.github.com>
2023-11-17 08:57:11 +0100

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,44 @@
+class player:
+    def __init__(self,name,age,rank):
+        self.name = name
+        self.age = age
+        self.rank = rank
+
+    def passs(self):
+        return
+    def shoot(self):
+        return
+    def jump(self):
+        return
+    
+
+
+class car:
+    def __init__(self,speed,color,nitroSpeed,model):
+        self.speed = speed
+        self.color = color
+        self.nitroSpeed = nitroSpeed
+        self.model = model
+
+    def turn(self):
+        return
+    def accelerate(self):
+        return
+    def brake(self):
+        return
+    def boost(self):
+        return
+    
+class car1:
+    def __init__(self,speed,color,enginePower,name):
+        self.speed = speed
+        self.color = color
+        self.nitroSpeed = enginePower
+        self.name = name
+
+    def start(self):
+        return
+    def stop(self):
+        return
+    def changegear(self):
+        return
